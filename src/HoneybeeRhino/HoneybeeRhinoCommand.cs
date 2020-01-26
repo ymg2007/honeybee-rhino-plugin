fa12d69184efb169b88d4ef1dd28c6fa489b5c24
MingboPeng <mingo1214@gmail.com>
2020-01-26 14:18:14 -0500

    feat(RhinoCommand): added rhino command to convert object in Rhino App

--- src/HoneybeeRhino/HoneybeeRhinoCommand.cs
+++ src/HoneybeeRhino/HoneybeeRhinoCommand.cs
@@ -5,6 +5,8 @@ using Rhino.Commands;
 using Rhino.Geometry;
 using Rhino.Input;
 using Rhino.Input.Custom;
+using Rhino.DocObjects;
+using System.Linq;
 
 namespace HoneybeeRhino
 {
@@ -31,44 +33,33 @@ namespace HoneybeeRhino
 
         protected override Result RunCommand(RhinoDoc doc, RunMode mode)
         {
-            // TODO: start here modifying the behaviour of your command.
-            // ---
-            RhinoApp.WriteLine("The {0} command will add a line right now.", EnglishName);
-
-            Point3d pt0;
-            using (GetPoint getPointAction = new GetPoint())
+            using (var go = new GetObject())
             {
-                getPointAction.SetCommandPrompt("Please select the start point");
-                if (getPointAction.Get() != GetResult.Point)
+                go.SetCommandPrompt("Please select a surface for converting to Honeybee Face3D");
+                go.GeometryFilter = ObjectType.Brep;
+                go.Get();
+                if (go.CommandResult() != Result.Success)
+                    return go.CommandResult();
+
+                if (go.ObjectCount == 0)
+                    return go.CommandResult();
+
+
+                var selectedObjs = go.Objects().Select(_=> _.Geometry() as Brep);
+                var hbFace3D = selectedObjs.Select(_ => _.ToHB());
+
+                if (selectedObjs.Count() == hbFace3D.Count())
                 {
-                    RhinoApp.WriteLine("No start point was selected.");
-                    return getPointAction.CommandResult();
+                    RhinoApp.WriteLine($"There are {hbFace3D.Count()} surfaces have been converted to Honeybee faces!");
+                    return Result.Success;
                 }
-                pt0 = getPointAction.Point();
+                else
+                {
+                    return Result.Failure;
+                }
+                
             }
 
-            Point3d pt1;
-            using (GetPoint getPointAction = new GetPoint())
-            {
-                getPointAction.SetCommandPrompt("Please select the end point");
-                getPointAction.SetBasePoint(pt0, true);
-                getPointAction.DynamicDraw +=
-                  (sender, e) => e.Display.DrawLine(pt0, e.CurrentPoint, System.Drawing.Color.DarkRed);
-                if (getPointAction.Get() != GetResult.Point)
-                {
-                    RhinoApp.WriteLine("No end point was selected.");
-                    return getPointAction.CommandResult();
-                }
-                pt1 = getPointAction.Point();
-            }
-
-            doc.Objects.AddLine(pt0, pt1);
-            doc.Views.Redraw();
-            RhinoApp.WriteLine("The {0} command added one line to the document.", EnglishName);
-
-            // ---
-
-            return Result.Success;
         }
     }
 }
