42a50ce3de24285158aca54a0b9942e8aa2d95a2
MingboPeng <mingo1214@gmail.com>
2020-01-26 12:55:52 -0500

    test(Converter): more rhino obj conversion tests

--- src/HoneybeeRhino.Test/Tests/ConvertToHB.cs
+++ src/HoneybeeRhino.Test/Tests/ConvertToHB.cs
@@ -25,5 +25,30 @@ namespace HoneybeeRhino.Test
             TestContext.WriteLine(string.Join(",", boudary[2]));
             Assert.AreEqual(boudary[2], new List<decimal> { 1, 2, 0});
         }
+
+        [Test]
+        public void Test_Surface()
+        {
+            var p = Plane.WorldXY;
+            var srf = new PlaneSurface(p, new Interval(0, 1), new Interval(0, 2)).ToBrep().Surfaces.First();
+
+            var face3D = srf.ToHB();
+            var boudary = face3D.Boundary;
+
+            TestContext.WriteLine(string.Join(",", boudary[2]));
+            Assert.AreEqual(boudary[2], new List<decimal> { 1, 2, 0 });
+        }
+
+        [Test]
+        public void Test_Brep()
+        {
+            BoundingBox box = new BoundingBox(new Point3d(0, 0, 0), new Point3d(1, 1, 1));
+            Brep brep = box.ToBrep();
+            var face3D = brep.ToHB();
+            var boudary = face3D.First().Boundary;
+
+            TestContext.WriteLine(string.Join(",", boudary[2]));
+            Assert.AreEqual(boudary[2], new List<decimal> { 1, 0, 1 });
+        }
     }
 }
