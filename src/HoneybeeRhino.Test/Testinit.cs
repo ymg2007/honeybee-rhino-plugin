3c6d4f83f2af19d3eb93b4c2f0c898c8f0957f68
MingboPeng <mingo1214@gmail.com>
2020-02-13 15:42:40 -0500

    fix(UnitTest): change RhinoCommon to 7.0

--- src/HoneybeeRhino.Test/Testinit.cs
+++ src/HoneybeeRhino.Test/Testinit.cs
@@ -17,7 +17,16 @@ namespace HoneybeeRhino.Test
         [OneTimeSetUp]
         public void Init()
         {
-            _doc = Rhino.RhinoDoc.ActiveDoc;
+            try
+            {
+                _doc = Rhino.RhinoDoc.ActiveDoc;
+            }
+            catch (DllNotFoundException e)
+            {
+
+                //throw;
+            }
+           
             if (initialized || _doc != null)
             {
                 return;
@@ -45,6 +54,7 @@ namespace HoneybeeRhino.Test
             // Add hook for .Net assmbly resolve (for RhinoCommmon.dll)
             AppDomain.CurrentDomain.AssemblyResolve += ResolveRhinoCommon;
 
+            Assembly.LoadFrom(@"D:\Dev\honeybee-rhino-plugin\src\HoneybeeRhino.Test\bin\Debug\HoneybeeRhino.dll");
             // Start headless Rhino process
             LaunchInProcess(0, 0);
         }
