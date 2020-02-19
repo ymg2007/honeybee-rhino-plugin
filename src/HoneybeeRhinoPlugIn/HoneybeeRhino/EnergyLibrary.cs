c85521ce7bea93c2f3b0f1d9a0c87df93f3de2a7
MingboPeng <mingo1214@gmail.com>
2020-02-18 21:03:06 -0500

    feat(RoomEnergyProperties): now can set all RoomEnergyProperties to room

--- src/HoneybeeRhinoPlugIn/HoneybeeRhino/EnergyLibrary.cs
+++ src/HoneybeeRhinoPlugIn/HoneybeeRhino/EnergyLibrary.cs
@@ -4,8 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using HB = HoneybeeSchema;
 
 namespace HoneybeeRhino
@@ -18,10 +16,21 @@ namespace HoneybeeRhino
 
         private static (string Url, string FilePath)[] _defaultLibraryFiles
             = new (string Url, string FilePath)[3] {
-                (_defaultConstructionSetUrl, "defaultConstructionSetUrl.json"),
-                (_defaultProgramTypesUrl, "defaultProgramTypesUrl.json"),
-                (_defaultHVACUrl, "defaultHVACUrl.json" )
+                (_defaultConstructionSetUrl, "defaultConstructionSets.json"),
+                (_defaultProgramTypesUrl, "defaultProgramTypes.json"),
+                (_defaultHVACUrl, "defaultHVACs.json" )
             };
+
+        private static string[] _LoadLibraries = new string[7]
+        {
+            "defaultPeopleLoads.json",
+            "defaultLightingLoads.json",
+            "defaultElectricEquipmentLoads.json",
+            "defaultGasEquipmentLoads.json",
+            "defaultInfiltrationLoads.json",
+            "defaultVentilationLoads.json",
+            "defaultSetpoints.json"
+        };
 
         public static string HoneybeeStandardFolder { get; } = Path.Combine(Path.GetTempPath(), "Ladybug", "Library");
 
@@ -40,11 +49,40 @@ namespace HoneybeeRhino
         public static IEnumerable<HB.IdealAirSystemAbridged> DefaultHVACs
             => _defaultHVACs ?? LoadLibrary(_defaultLibraryFiles[2].FilePath, HB.IdealAirSystemAbridged.FromJson);
 
-        //public static IEnumerable<string> DownloadAllDefaultLibraries()
-        //{
-        //    var files = _defaultLibraryFiles.Select(_ => DownLoadLibrary(_.Url, _.FilePath));
-        //    return files;
-        //}
+        //People load
+        private static IEnumerable<HB.PeopleAbridged> _defaultPeopleLoads;
+        public static IEnumerable<HB.PeopleAbridged> DefaultPeopleLoads
+            => _defaultPeopleLoads ?? LoadLibrary(_LoadLibraries[0], HB.PeopleAbridged.FromJson);
+
+        //Lighting load
+        private static IEnumerable<HB.LightingAbridged> _defaultLightingLoads;
+        public static IEnumerable<HB.LightingAbridged> DefaultLightingLoads
+            => _defaultLightingLoads ?? LoadLibrary(_LoadLibraries[1], HB.LightingAbridged.FromJson);
+
+        //ElecEqp load
+        private static IEnumerable<HB.ElectricEquipmentAbridged> _defaultElectricEquipmentLoads;
+        public static IEnumerable<HB.ElectricEquipmentAbridged> DefaultElectricEquipmentLoads
+            => _defaultElectricEquipmentLoads ?? LoadLibrary(_LoadLibraries[2], HB.ElectricEquipmentAbridged.FromJson);
+
+        //GasEqp load
+        private static IEnumerable<HB.GasEquipmentAbridged> _defaultGasEquipmentLoads;
+        public static IEnumerable<HB.GasEquipmentAbridged> GasEquipmentLoads
+            => _defaultGasEquipmentLoads ?? LoadLibrary(_LoadLibraries[3], HB.GasEquipmentAbridged.FromJson);
+
+        //GasEqp load
+        private static IEnumerable<HB.InfiltrationAbridged> _defaultInfiltrationLoads;
+        public static IEnumerable<HB.InfiltrationAbridged> DefaultInfiltrationLoads
+            => _defaultInfiltrationLoads ?? LoadLibrary(_LoadLibraries[4], HB.InfiltrationAbridged.FromJson);
+
+        //Ventilation load
+        private static IEnumerable<HB.VentilationAbridged> _defaultVentilationLoads;
+        public static IEnumerable<HB.VentilationAbridged> DefaultVentilationLoads
+            => _defaultVentilationLoads ?? LoadLibrary(_LoadLibraries[5], HB.VentilationAbridged.FromJson);
+
+        //Setpoints
+        private static IEnumerable<HB.SetpointAbridged> _defaultSetpoints;
+        public static IEnumerable<HB.SetpointAbridged> DefaultSetpoints
+            => _defaultSetpoints ?? LoadLibrary(_LoadLibraries[6], HB.SetpointAbridged.FromJson);
 
         public static string DownLoadLibrary(string standardsUrl, string saveAsfileName)
         {
@@ -74,6 +112,7 @@ namespace HoneybeeRhino
 
         public static IEnumerable<T> LoadLibrary<T>(string jsonFilename, Func<string,T> func)
         {
+            //TODO: remove this later for real deployment
             //Check or download all json libraries.
             CheckAllDefaultLibraries();
 
@@ -82,33 +121,17 @@ namespace HoneybeeRhino
             if (!File.Exists(jsonFile))
                 throw new ArgumentException($"Invalid file: {jsonFile}");
 
-            using (StreamReader file = File.OpenText(jsonFile))
+            using (var file = File.OpenText(jsonFile))
+            using(var reader = new JsonTextReader(file))
             {
-                var json = file.ReadToEnd();
+                var jObjs = (JObject)JToken.ReadFrom(reader);
+                var libItems = jObjs.Values();
 
-                var hbObj = func(json);
-                return new T[1] { hbObj };
+                var hbObjs = libItems.Select(_ => func(_.ToString()));
+                return hbObjs;
             }
                
         }
 
-        //public static IEnumerable<HB.ProgramTypeAbridged> LoadProgramTypes(string jsonFile)
-        //{
-        //    if (!File.Exists(jsonFile))
-        //        throw new ArgumentException($"Invalid file: {jsonFile}");
-
-        //    using (StreamReader file = File.OpenText(jsonFile))
-        //    {
-        //        using (JsonTextReader reader = new JsonTextReader(file))
-        //        {
-        //            JObject o2 = (JObject)JToken.ReadFrom(reader);
-        //            var programs = o2.Values();
-        //            var st = programs.First().ToString();
-        //            return programs.Select(_ => HB.ProgramTypeAbridged.FromJson(_.ToString()));
-        //        }
-
-        //    }
-
-        //}
     }
 }
--- src/HoneybeeRhinoPlugIn/UI/EnergyPropertyDialog.cs
+++ src/HoneybeeRhinoPlugIn/UI/EnergyPropertyDialog.cs
@@ -1,5 +1,6 @@
 ﻿using Eto.Drawing;
 using Eto.Forms;
+using System.Collections.Generic;
 using System.Linq;
 using HB = HoneybeeSchema;
 
@@ -10,19 +11,36 @@ namespace HoneybeeRhino.UI
         private HB.ConstructionSetAbridged _constructionSet;
         private HB.ProgramTypeAbridged _programType;
         private HB.IdealAirSystemAbridged _idealAirSystem;
-
+        private HB.PeopleAbridged _people;
+        private HB.LightingAbridged _lighting;
+        private HB.ElectricEquipmentAbridged _elecEqp;
+        private HB.GasEquipmentAbridged _gasEqp;
+        private HB.InfiltrationAbridged _infilt;
+        private HB.VentilationAbridged _vent;
+        private HB.SetpointAbridged _setpoint;
         public EnergyPropertyDialog()
         {
             Padding = new Padding(5);
             Resizable = true;
             Title = "Honeybee Rhino PlugIn";
             WindowStyle = WindowStyle.Default;
-            MinimumSize = new Size(400, 300);
+            MinimumSize = new Size(450, 600);
 
             DefaultButton = new Button { Text = "OK" };
-            DefaultButton.Click += (sender, e) 
+            DefaultButton.Click += (sender, e)
                 => Close(
-                new HB.RoomEnergyPropertiesAbridged(constructionSet: this._constructionSet?.Name, programType: this._programType?.Name, hvac: this._idealAirSystem?.Name)
+                new HB.RoomEnergyPropertiesAbridged(
+                    constructionSet: this._constructionSet?.Name, 
+                    programType: this._programType?.Name, 
+                    hvac: this._idealAirSystem?.Name,
+                    people: this._people,
+                    lighting: this._lighting,
+                    electricEquipment:this._elecEqp,
+                    gasEquipment: this._gasEqp,
+                    infiltration: this._infilt,
+                    ventilation: this._vent,
+                    setpoint: this._setpoint
+                    )
                 );
 
             AbortButton = new Button { Text = "Cancel" };
@@ -33,30 +51,66 @@ namespace HoneybeeRhino.UI
             var constructionSetDropdown = new DropDown();
             constructionSetDropdown.Items.AddRange(constructions);
             constructionSetDropdown.SelectedIndexChanged +=
-                (s, e) => this._constructionSet = (constructionSetDropdown.Items[constructionSetDropdown.SelectedIndex] as ListItem).Tag as HoneybeeSchema.ConstructionSetAbridged;
+                (s, e) => this._constructionSet = (constructionSetDropdown.Items[constructionSetDropdown.SelectedIndex] as ListItem).Tag as HB.ConstructionSetAbridged;
 
 
             //Get programs
-            var programs = EnergyLibrary.DefaultProgramTypes.Select(_=> new ListItem() { Text = _.Name, Tag = _ }).OrderBy(_=>_.Text);
+            var programs = EnergyLibrary.DefaultProgramTypes.Select(_ => new ListItem() { Text = _.Name, Tag = _ }).OrderBy(_ => _.Text);
             var programTypesDropdown = new DropDown();
             programTypesDropdown.Items.AddRange(programs);
-            programTypesDropdown.SelectedIndexChanged += 
-                (s, e) => this._programType = (programTypesDropdown.Items[programTypesDropdown.SelectedIndex] as ListItem).Tag as HoneybeeSchema.ProgramTypeAbridged;
+            programTypesDropdown.SelectedIndexChanged +=
+                (s, e) => this._programType = (programTypesDropdown.Items[programTypesDropdown.SelectedIndex] as ListItem).Tag as HB.ProgramTypeAbridged;
 
-            
+
             //Get HVACs
             var hvacs = EnergyLibrary.DefaultHVACs.Select(_ => new ListItem() { Text = _.Name, Tag = _ }).OrderBy(_ => _.Text);
             var hvacsDropdown = new DropDown();
             hvacsDropdown.Items.AddRange(hvacs);
             hvacsDropdown.SelectedIndexChanged +=
-                (s, e) => this._idealAirSystem = (hvacsDropdown.Items[hvacsDropdown.SelectedIndex] as ListItem).Tag as HoneybeeSchema.IdealAirSystemAbridged;
+                (s, e) => this._idealAirSystem = (hvacsDropdown.Items[hvacsDropdown.SelectedIndex] as ListItem).Tag as HB.IdealAirSystemAbridged;
+
+            var defaultByProgramType = "By Default Program Type";
+            //Get people
+            var peopleDP = MakeDropDown(EnergyLibrary.DefaultPeopleLoads, defaultByProgramType);
+            peopleDP.SelectedIndexChanged +=
+                (s, e) => this._people = (peopleDP.Items[peopleDP.SelectedIndex] as ListItem).Tag as HB.PeopleAbridged;
+
+            //Get lighting
+            var lightingDP = MakeDropDown(EnergyLibrary.DefaultLightingLoads, defaultByProgramType);
+            lightingDP.SelectedIndexChanged +=
+                (s, e) => this._lighting = (lightingDP.Items[lightingDP.SelectedIndex] as ListItem).Tag as HB.LightingAbridged;
+
+            //Get ElecEqp
+            var elecEqpDP = MakeDropDown(EnergyLibrary.DefaultElectricEquipmentLoads, defaultByProgramType);
+            elecEqpDP.SelectedIndexChanged +=
+                (s, e) => this._elecEqp = (elecEqpDP.Items[elecEqpDP.SelectedIndex] as ListItem).Tag as HB.ElectricEquipmentAbridged;
+
+            //Get gasEqp
+            var gasEqpDP = MakeDropDown(EnergyLibrary.GasEquipmentLoads, defaultByProgramType);
+            gasEqpDP.SelectedIndexChanged +=
+                (s, e) => this._gasEqp = (gasEqpDP.Items[gasEqpDP.SelectedIndex] as ListItem).Tag as HB.GasEquipmentAbridged;
+
+            //Get infiltration
+            var infilDP = MakeDropDown(EnergyLibrary.DefaultInfiltrationLoads, defaultByProgramType);
+            infilDP.SelectedIndexChanged +=
+                (s, e) => this._infilt = (infilDP.Items[infilDP.SelectedIndex] as ListItem).Tag as HB.InfiltrationAbridged;
+
+            //Get ventilation
+            var ventDP = MakeDropDown(EnergyLibrary.DefaultVentilationLoads, defaultByProgramType);
+            ventDP.SelectedIndexChanged +=
+                (s, e) => this._vent = (ventDP.Items[ventDP.SelectedIndex] as ListItem).Tag as HB.VentilationAbridged;
+
+            //Get setpoint
+            var setPtDP = MakeDropDown(EnergyLibrary.DefaultSetpoints, defaultByProgramType);
+            setPtDP.SelectedIndexChanged +=
+                (s, e) => this._setpoint = (setPtDP.Items[setPtDP.SelectedIndex] as ListItem).Tag as HB.SetpointAbridged;
 
 
             var buttons = new TableLayout
             {
                 Padding = new Padding(5, 10, 5, 5),
                 Spacing = new Size(10, 10),
-                Rows = { new TableRow(null,this.DefaultButton, this.AbortButton,null) }
+                Rows = { new TableRow(null, this.DefaultButton, this.AbortButton, null) }
             };
 
             //Create layout
@@ -72,6 +126,14 @@ namespace HoneybeeRhino.UI
                     programTypesDropdown,
                     new Label(){ Text = "Default HVAC:"},
                     hvacsDropdown,
+                    new Label(){ Text = " "},
+                    new Label(){ Text = "People [ppl/m2]:"}, peopleDP,
+                    new Label(){ Text = "Lighting [W/m2]:"}, lightingDP,
+                    new Label(){ Text = "Electric Equipment [W/m2]:"}, elecEqpDP,
+                    new Label(){ Text = "Gas Equipment [W/m2]:"}, gasEqpDP,
+                    new Label(){ Text = "Infiltration [m3/s per m2 facade @4Pa]:"}, infilDP,
+                    new Label(){ Text = "Ventilation [m3/s.m2]:"}, ventDP,
+                    new Label(){ Text = "Setpoint [C]:"}, setPtDP,
                     new TableRow(buttons),
                     null
                 }
@@ -80,5 +142,21 @@ namespace HoneybeeRhino.UI
 
         }
 
+        private DropDown MakeDropDown<T>(IEnumerable<T> Library, string defaultItem) where T : HB.IAbridged
+        {
+
+            var dropdownItems = Library.Select(_ => new ListItem() { Text = _.Name, Tag = _ }).OrderBy(_ => _.Text);
+            var dp = new DropDown();
+
+            if (!string.IsNullOrEmpty(defaultItem))
+            {
+                dp.Items.Add(defaultItem);
+                dp.SelectedIndex = 0;
+            }
+            
+            dp.Items.AddRange(dropdownItems);
+           
+            return dp;
+        }
     }
 }
