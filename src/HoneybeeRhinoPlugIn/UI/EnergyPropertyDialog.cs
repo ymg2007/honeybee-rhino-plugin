2de09b6822cf6f62618f194af8fff24b15bb39b5
MingboPeng <mingo1214@gmail.com>
2020-02-27 16:30:47 -0500

    refactor(EnergyProperty): do it smarter way.

--- src/HoneybeeRhinoPlugIn/UI/EnergyPropertyDialog.cs
+++ src/HoneybeeRhinoPlugIn/UI/EnergyPropertyDialog.cs
@@ -21,109 +21,49 @@ namespace HoneybeeRhino.UI
             MinimumSize = new Size(450, 620);
 
             //Get constructions
-            var constrSets = EnergyLibrary.DefaultConstructionSets.ToList();
-            var constructionSetDP = MakeDropDown(constrSets, "By Global Model ConstructionSet");
-            constructionSetDP.SelectedIndexBinding.Bind(
-                   () => constrSets.FindIndex(_ => _.Name == EnergyProp.ConstructionSet) + 1,
-                   (int i) => EnergyProp.ConstructionSet = i == 0 ? null : constrSets[i - 1].Name
-                   );
+            var constructionSetDP = MakeDropDown(EnergyProp.ConstructionSet, (v) => EnergyProp.ConstructionSet = v.Name,
+                EnergyLibrary.DefaultConstructionSets, "By Global Model ConstructionSet");
 
-            //tb.Rows.Add(new Label() { Text = "Room ConstructionSet:" });
-            //tb.Rows.Add(constructionSetDP);
 
             //Get programs
-            var prgSets = EnergyLibrary.DefaultProgramTypes.ToList();
-            var programTypesDP = MakeDropDown(prgSets, "Unoccupied, NoLoads");
-            programTypesDP.SelectedIndexBinding.Bind(
-                  () => prgSets.FindIndex(_ => _.Name == EnergyProp.ProgramType) + 1,
-                  (int i) => EnergyProp.ProgramType = i == 0 ? null : prgSets[i - 1].Name
-                  );
-            //tb.Rows.Add(new Label() { Text = "Room Program Type:" });
-            //tb.Rows.Add(programTypesDP);
+            var programTypesDP = MakeDropDown(EnergyProp.ProgramType, (v) => EnergyProp.ProgramType = v.Name,
+               EnergyLibrary.DefaultProgramTypes, "Unoccupied, NoLoads");
 
             //Get HVACs
-            var hvacSets = EnergyLibrary.DefaultHVACs.ToList();
-            var hvacDP = MakeDropDown( hvacSets, "Unconditioned");
-            hvacDP.SelectedIndexBinding.Bind(
-                  () => hvacSets.FindIndex(_ => _.Name == EnergyProp.Hvac) + 1,
-                  (int i) => EnergyProp.Hvac = i == 0 ? null : hvacSets[i - 1].Name
-                  );
-            //tb.Rows.Add(new Label() { Text = "Room HVAC:" });
-            //tb.Rows.Add(hvacDP);
+            var hvacDP = MakeDropDown(EnergyProp.Hvac, (v) => EnergyProp.Hvac = v.Name,
+               EnergyLibrary.DefaultHVACs, "Unconditioned"); 
+
 
             var defaultByProgramType = "By Room Program Type";
             //Get people
-            var ppls= EnergyLibrary.DefaultPeopleLoads.ToList();
-            var peopleDP = MakeDropDown( ppls, defaultByProgramType);
-            peopleDP.SelectedIndexBinding.Bind(
-                () => ppls.FindIndex(_ => _.Name == EnergyProp.People?.Name) + 1,
-                (int i) => EnergyProp.People = i == 0 ? null : ppls[i - 1]
-                );
-            //tb.Rows.Add(new Label() { Text = "People [ppl/m2]:" });
-            //tb.Rows.Add(peopleDP);
+            var peopleDP = MakeDropDown(EnergyProp.People, (v) => EnergyProp.People = v,
+                EnergyLibrary.DefaultPeopleLoads, defaultByProgramType);
 
             //Get lighting
-            var lpds = EnergyLibrary.DefaultLightingLoads.ToList();
-            var lightingDP = MakeDropDown(lpds, defaultByProgramType);
-            lightingDP.SelectedIndexBinding.Bind(
-                () => lpds.FindIndex(_ => _.Name == EnergyProp.Lighting?.Name) + 1,
-                (int i) => EnergyProp.Lighting = i == 0 ? null : lpds[i - 1]
-                );
-            //tb.Rows.Add(new Label() { Text = "Lighting [W/m2]:" });
-            //tb.Rows.Add(lightingDP);
+            var lightingDP = MakeDropDown(EnergyProp.Lighting, (v) => EnergyProp.Lighting = v,
+                EnergyLibrary.DefaultLightingLoads, defaultByProgramType);
 
             //Get ElecEqp
-            var eqps = EnergyLibrary.DefaultElectricEquipmentLoads.ToList();
-            var elecEqpDP = MakeDropDown(eqps, defaultByProgramType);
-            elecEqpDP.SelectedIndexBinding.Bind(
-                () => eqps.FindIndex(_ => _.Name == EnergyProp.ElectricEquipment?.Name) + 1,
-                (int i) => EnergyProp.ElectricEquipment = i == 0 ? null : eqps[i - 1]
-                );
-            //tb.Rows.Add(new Label() { Text = "Electric Equipment [W/m2]:" });
-            //tb.Rows.Add(elecEqpDP);
+            var elecEqpDP = MakeDropDown(EnergyProp.ElectricEquipment, (v) => EnergyProp.ElectricEquipment = v,
+                EnergyLibrary.DefaultElectricEquipmentLoads, defaultByProgramType);
 
             //Get gasEqp
-            var gases = EnergyLibrary.GasEquipmentLoads.ToList();
-            var gasEqpDP = MakeDropDown(gases, defaultByProgramType);
-            gasEqpDP.SelectedIndexBinding.Bind(
-                () => gases.FindIndex(_ => _.Name == EnergyProp.GasEquipment?.Name) + 1,
-                (int i) => EnergyProp.GasEquipment = i == 0 ? null : gases[i - 1]
-                );
-            //tb.Rows.Add(new Label() { Text = "Gas Equipment [W/m2]:" });
-            //tb.Rows.Add(gasEqpDP);
+            var gasEqpDP = MakeDropDown(EnergyProp.GasEquipment, (v) => EnergyProp.GasEquipment = v,
+                EnergyLibrary.GasEquipmentLoads, defaultByProgramType);
 
             //Get infiltration
-            var infls = EnergyLibrary.DefaultInfiltrationLoads.ToList();
-            var infilDP = MakeDropDown(infls, defaultByProgramType);
-            infilDP.SelectedIndexBinding.Bind(
-                () => infls.FindIndex(_ => _.Name == EnergyProp.Infiltration?.Name) + 1,
-                (int i) => EnergyProp.Infiltration = i == 0 ? null : infls[i - 1]
-                );
-            //tb.Rows.Add(new Label() { Text = "Infiltration [m3/s per m2 facade @4Pa]:" });
-            //tb.Rows.Add(infilDP);
+            var infilDP = MakeDropDown(EnergyProp.Infiltration, (v) => EnergyProp.Infiltration = v,
+                EnergyLibrary.DefaultInfiltrationLoads, defaultByProgramType);
 
 
             //Get ventilation
-            var vents = EnergyLibrary.DefaultVentilationLoads.ToList();
-            var ventDP = MakeDropDown(vents, defaultByProgramType);
-            ventDP.SelectedIndexBinding.Bind(
-                () => vents.FindIndex(_ => _.Name == EnergyProp.Ventilation?.Name) + 1,
-                (int i) => EnergyProp.Ventilation = i == 0 ? null : vents[i - 1]
-                );
-            //tb.Rows.Add(new Label() { Text = "Ventilation [m3/s]:" });
-            //tb.Rows.Add(ventDP);
+            var ventDP = MakeDropDown(EnergyProp.Ventilation, (v) => EnergyProp.Ventilation = v,
+                EnergyLibrary.DefaultVentilationLoads, defaultByProgramType);
 
             //Get setpoint
-            var stps = EnergyLibrary.DefaultSetpoints.ToList();
-            var setPtDP = MakeDropDown(stps, defaultByProgramType);
-            setPtDP.SelectedIndexBinding.Bind(
-                () => stps.FindIndex(_ => _.Name == EnergyProp.Setpoint?.Name) + 1,
-                (int i) => EnergyProp.Setpoint = i == 0 ? null : stps[i - 1]
-                );
-            //tb.Rows.Add(new Label() { Text = "Setpoint [C]:" });
-            //tb.Rows.Add(ventDP);
-
-
+            var setPtDP = MakeDropDown(EnergyProp.Setpoint, (v) => EnergyProp.Setpoint = v,
+                EnergyLibrary.DefaultSetpoints, defaultByProgramType);
+   
 
             DefaultButton = new Button { Text = "OK" };
             DefaultButton.Click += (sender, e) => Close(EnergyProp);
@@ -163,17 +103,16 @@ namespace HoneybeeRhino.UI
             };
             
         }
-
-        private DropDown MakeDropDown<T>(List<T> Library, string defaultItemName = default) where T : HB.INamed
+        private DropDown MakeDropDown<T>(string currentObjName, Action<T> setAction, IEnumerable<T> valueLibrary, string defaultItemName = default) where T : HB.INamed
         {
-            var items = Library;
+            var items = valueLibrary.ToList();
             var dropdownItems = items.Select(_ => new ListItem() { Text = _.Name, Tag = _ }).ToList();
             var dp = new DropDown();
 
             if (!string.IsNullOrEmpty(defaultItemName))
             {
                 var foundIndex = dropdownItems.FindIndex(_ => _.Text == defaultItemName);
-                
+
                 if (foundIndex > -1)
                 {
                     //Add exist item from list
@@ -188,53 +127,50 @@ namespace HoneybeeRhino.UI
 
             }
 
-            ////Set to saved item
-            //if (bindObj != null)
-            //{
-            //    var selIndex = dp.SelectedIndex;
-            //}
-            
-
             dp.Items.AddRange(dropdownItems);
 
+            dp.SelectedIndexBinding.Bind(
+                () => items.FindIndex(_ => _.Name == currentObjName) + 1,
+                (int i) => setAction(i == 0 ? items[i] : items[i - 1])
+                );
+
             return dp;
-            //Action<int> func = (int i) =>
-            //{
-            //    if (bindObj is string)
-            //    {
-            //        bindObj = i == 0 ? default : items[i - 1].Name;
-            //    }
-            //    else
-            //    {
-            //        bindObj = i == 0 ? default : items[i - 1];
-            //    }
-
-            //};
-            //dp.SelectedIndexBinding.Bind(
-            //    () => items.FindIndex(_ => _.Name == dp.SelectedKey) + 1,
-            //    (int i)=> bindObj = i == 0 ? default : items[i - 1]
-            //    );
-            //return dp;
-
-            //void setValueByIndex(int i)
-            //{
-            //    SetValue(ref bindObj, items[i - 1]);
-
-            //};
-
 
         }
+        private DropDown MakeDropDown<T>(T currentValue, Action<T> setAction, IEnumerable<T> valueLibrary, string defaultItemName = default) where T : HB.INamed
+        {
+            return MakeDropDown(currentValue?.Name, setAction, valueLibrary, defaultItemName);
+        }
 
-        //private void SetValue<T>(ref object bindObj, T item)
+        //private DropDown MakeDropDown<T>(List<T> Library, string defaultItemName = default) where T : HB.INamed
         //{
-        //    if (bindObj is string)
+        //    var items = Library;
+        //    var dropdownItems = items.Select(_ => new ListItem() { Text = _.Name, Tag = _ }).ToList();
+        //    var dp = new DropDown();
+
+        //    if (!string.IsNullOrEmpty(defaultItemName))
         //    {
-        //        bindObj = i == 0 ? default : items[i - 1].Name;
+        //        var foundIndex = dropdownItems.FindIndex(_ => _.Text == defaultItemName);
+
+        //        if (foundIndex > -1)
+        //        {
+        //            //Add exist item from list
+        //            dp.SelectedIndex = foundIndex;
+        //        }
+        //        else
+        //        {
+        //            //Add a default None item with a name
+        //            dp.Items.Add(defaultItemName);
+        //            dp.SelectedIndex = 0;
+        //        }
+
         //    }
-        //    else
-        //    {
-        //        bindObj = i == 0 ? default : items[i - 1];
-        //    }
+
+        //    dp.Items.AddRange(dropdownItems);
+
+        //    return dp;
+
+
         //}
     }
 }
