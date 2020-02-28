6c2903887437d41e3094c5413657d7a8cf93dc9d
MingboPeng <mingo1214@gmail.com>
2020-02-27 22:36:52 -0500

    fix(CleanUp): remove unused sample codes

--- src/HoneybeeRhinoPlugIn/UI/PropertyPanel.cs
+++ src/HoneybeeRhinoPlugIn/UI/PropertyPanel.cs
@@ -96,62 +96,6 @@ namespace HoneybeeRhino.UI
             layout.AddSeparateRow(multiplierNum);
 
 
-            //var props = typeof(Room).GetProperties();
-
-            
-            //foreach (var p in props)
-            //{
-            //    var pType = p.PropertyType;
-            //    if (pType.IsGenericType && (pType.GetGenericTypeDefinition() ==(typeof(List<>))))
-            //    {
-            //        layout.AddSeparateRow(new Label { Text = p.Name });
-            //        //var t = new TextBox { Text = p.GetValue(room) as string };
-            //        var t2 = new ListBox();
-            //        var items = (IList)p.GetValue(room);
-            //        t2.Height = 50;
-            //        if (items == null)
-            //            continue;
-
-            //        foreach (var item in items)
-            //        {
-            //            var value = nameof(item);
-            //            if (item is Face f)
-            //            {
-            //                t2.Items.Add(new ListItem() { Text = f.DisplayName??f.Name });
-            //            }
-            //            else
-            //            {
-            //                t2.Items.Add(new ListItem() { Text = value });
-            //            }
-                        
-            //        }
-
-            //        if (items.Count > 0)
-            //        {
-            //            t2.Height = 80;
-            //        }
-            //        layout.AddSeparateRow(t2);
-            //    }
-            //    else
-            //    {
-            //        layout.AddSeparateRow(new Label { Text = p.Name });
-            //        var propValue = p.GetValue(room);
-            //        TextControl t;
-            //        if (propValue is RoomPropertiesAbridged rmProp)
-            //        {
-            //            var rmPropBtn = new Button { Text = "Room Analytical Properties" };
-            //            rmPropBtn.Click += (s,e)=> RmPropBtn_Click(rmProp.Energy);
-            //            t = rmPropBtn;
-            //        }
-            //        else
-            //        {
-            //            t = new TextBox { Text = propValue?.ToString() };
-            //        }
-           
-            //        layout.AddSeparateRow(t);
-            //    }
-            //}
-            
 
             layout.Add(null);
             var data_button = new Button { Text = "Honeybee Data" };
@@ -177,42 +121,5 @@ namespace HoneybeeRhino.UI
 
         }
 
-        protected void OnHelloButton()
-        {
-            // Use the Rhino common message box and NOT the Eto MessageBox,
-            // the Eto version expects a top level Eto Window as the owner for
-            // the MessageBox and will cause problems when running on the Mac.
-            // Since this panel is a child of some Rhino container it does not
-            // have a top level Eto Window.
-            Dialogs.ShowMessage("Hello Rhino!", "Sample");
-        }
-
-        /// <summary>
-        /// Sample of how to display a child Eto dialog
-        /// </summary>
-        protected void OnChildButton()
-        {
-            var dialog = new SampleCsEtoHelloWorld();
-            dialog.ShowModal(this);
-        }
-
-        class SampleCsEtoHelloWorld : CommandDialog
-        {
-            public SampleCsEtoHelloWorld()
-            {
-                Padding = new Padding(10);
-                Title = "Hello World";
-                Resizable = false;
-                Content = new StackLayout()
-                {
-                    Padding = new Padding(0),
-                    Spacing = 6,
-                    Items =
-                    {
-                      new Label { Text="This is a child dialog..." }
-                    }
-                };
-            }
-        }
     }
 }
