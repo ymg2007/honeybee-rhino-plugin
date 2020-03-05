edaeeda925a233bf85d75da272d10a596652f2d7
MingboPeng <mingo1214@gmail.com>
2020-03-05 11:25:05 -0500

    feat(addWindowByWWR): add wwr for multiple rooms, add room checks

--- src/HoneybeeRhinoPlugIn/RhinoCommands/HB_AddWindowByWWR.cs
+++ src/HoneybeeRhinoPlugIn/RhinoCommands/HB_AddWindowByWWR.cs
@@ -35,8 +35,13 @@ namespace HoneybeeRhino.RhinoCommands
             {
                 using (var go = new GetObject())
                 {
-                    var wwr = Settings.GetDouble("WWR", 0.6);
-                    OptionDouble optionWWR = new OptionDouble(wwr, 0.01, 0.98);
+                    var wwr = 0.6;
+                    wwr = Settings.GetDouble(nameof(wwr), 0.6);
+                    var optionWWR = new OptionDouble(wwr, 0.01, 0.98);
+
+                    //var ignoreExistingWindow = false;
+                    //ignoreExistingWindow = Settings.GetBool(nameof(ignoreExistingWindow), false);
+                    //var optionIgnoreExistingWindow = new OptionToggle(ignoreExistingWindow,"apply to all faces","ignore faces with existing windows");
 
                     go.SetCommandPrompt("Please select honeybee rooms for adding windows to");
                     go.GeometryFilter = ObjectType.Brep;
@@ -51,11 +56,9 @@ namespace HoneybeeRhino.RhinoCommands
                     while (true)
                     {
                         go.ClearCommandOptions();
-
-
                         go.AddOptionDouble("WindowWallRatio", ref optionWWR);
-
-                        var res = go.Get();
+                        //go.AddOptionToggle(nameof(ignoreExistingWindow), ref optionIgnoreExistingWindow);
+                        var res = go.GetMultiple(1, 0);
 
                         if (res == Rhino.Input.GetResult.Option)
                         {
@@ -81,69 +84,31 @@ namespace HoneybeeRhino.RhinoCommands
                         throw new ArgumentException("Failed to execute command!");
 
                     if (go.ObjectCount == 0)
-                        throw new ArgumentException("No objects are selected!");
+                        throw new ArgumentException("No object is selected!");
 
-                    Settings.SetDouble("WWR", optionWWR.CurrentValue);
+                    Settings.SetDouble(nameof(wwr), optionWWR.CurrentValue);
+                    //Settings.SetBool(nameof(ignoreExistingWindow), optionIgnoreExistingWindow.CurrentValue);
 
                     //all selected room geometries
-                    var rooms = go.Objects();
-
-                    //Get Room geometry guid
-                    var roomIds = go.Objects().Select(_ => _.ObjectId);
-
-                    //Get all outdoor faces
-                    var room = rooms.First();
-                    var ourdoorWalls = room.Brep().Faces.Where(_ => {
-                        var face = _.TryGetFaceEntity().HBObject;
-                        var isOutdoor = face.BoundaryCondition.Obj is HoneybeeSchema.Outdoors;
-                        var isWall = face.FaceType == HoneybeeSchema.Face.FaceTypeEnum.Wall;
-                        return isOutdoor && isWall;
-                    });
-
-                    //prepare BrepObjects
-                    //create window
-                    var apertures = new List<ObjRef>();
-                    //Brep newRoomWithApt = null;
-                    foreach (var wall in ourdoorWalls)
+                    var solidBreps = go.Objects().Where(_ => _.Brep() != null).Where(_ => _.Brep().IsSolid);
+                    var rooms = solidBreps.Where(_ => _.IsRoom()).ToList();
+                    if (solidBreps.Count() != rooms.Count())
                     {
-                        var apt = wall.DuplicateFace(false);
-                        var c = AreaMassProperties.Compute(apt).Centroid;
-                        var ts = Transform.Scale(c, Math.Sqrt(optionWWR.CurrentValue));
-                        var success = apt.Transform(ts);
-                        if (!success)
-                            continue;
+                        doc.Objects.UnselectAll();
+                        var nonRooms = solidBreps.Where(_ => !_.Brep().IsRoom());
+                        foreach (var item in nonRooms)
+                        {
+                            doc.Objects.Select(item, true, true);
+                        }
 
-                        var id = doc.Objects.AddBrep(apt);
-                        var aperture = new ObjRef(id);
-                        apertures.Add(aperture);
-
-                    }
-
-                    //match window to room 
-                    var matchedRoomApt = room.AddApertures(apertures);
-                    //TODO: figure out remove those are unsuccessfully added.
-                    //if (matchedRoomApt.apertures == null)
-                    //{
-                    //    //Failed to add 
-                    //    doc.Objects.Delete(id, true);
-                    //    continue;
-                    //}
-
-                    //by here, two lists are equal length
-                    var apts = matchedRoomApt.apertures;
-                    foreach (var item in apts)
-                    {
-                        doc.Objects.Replace(item.id, item.brep);
-                    }
-                    var newRoomWithApt = matchedRoomApt.room;
-                    
-
-
-                    if (newRoomWithApt == null)
+                        doc.Views.Redraw();
+                        Rhino.UI.Dialogs.ShowMessage("These are not Honeybee rooms, please use MassToRoom to convert them first!", "Honeybee Rhino Plugin");
                         return Result.Failure;
+                    }
 
-                    //Replace the rhino object in order to be able to undo/redo
-                    doc.Objects.Replace(room.ObjectId, newRoomWithApt);
+
+                    //Add Windows
+                    AddApertureByWWR(doc, rooms, optionWWR.CurrentValue);
 
                     doc.Views.Redraw();
                     return Result.Success;
@@ -156,6 +121,69 @@ namespace HoneybeeRhino.RhinoCommands
                 return Result.Failure;
             }
             
+        }
+        public void AddApertureByWWR(RhinoDoc doc, IEnumerable<ObjRef> rooms, double wwr)
+        {
+            foreach (var room in rooms)
+            {
+                AddApertureByWWR(doc, room, wwr);
+            }
+        }
+
+        public void AddApertureByWWR(RhinoDoc doc, ObjRef roomObjRef, double wwr)
+        {
+            //Get all outdoor faces
+            var room = roomObjRef;
+            var ourdoorWalls = room.Brep().Faces.Where(_ => {
+                var face = _.TryGetFaceEntity().HBObject;
+                var isOutdoor = face.BoundaryCondition.Obj is HoneybeeSchema.Outdoors;
+                var isWall = face.FaceType == HoneybeeSchema.Face.FaceTypeEnum.Wall;
+                return isOutdoor && isWall;
+            });
+
+            //prepare BrepObjects
+            //create window
+            var apertures = new List<ObjRef>();
+            //Brep newRoomWithApt = null;
+            foreach (var wall in ourdoorWalls)
+            {
+                var apt = wall.DuplicateFace(false);
+                var c = AreaMassProperties.Compute(apt).Centroid;
+                var ts = Transform.Scale(c, Math.Sqrt(wwr));
+                var success = apt.Transform(ts);
+                if (!success)
+                    continue;
+
+                var id = doc.Objects.AddBrep(apt);
+                var aperture = new ObjRef(id);
+                apertures.Add(aperture);
+
+            }
+
+            //match window to room 
+            var matchedRoomApt = room.AddApertures(apertures);
+            //TODO: figure out remove those are unsuccessfully added.
+            //if (matchedRoomApt.apertures == null)
+            //{
+            //    //Failed to add 
+            //    doc.Objects.Delete(id, true);
+            //    continue;
+            //}
+
+            //by here, two lists are equal length
+            var apts = matchedRoomApt.apertures;
+            foreach (var item in apts)
+            {
+                doc.Objects.Replace(item.id, item.brep);
+            }
+
+            var newRoomWithApt = matchedRoomApt.room;
+            if (newRoomWithApt != null)
+            {
+                //Replace the rhino object in order to be able to undo/redo
+                doc.Objects.Replace(room.ObjectId, newRoomWithApt);
+            }
+
         }
     }
 }
